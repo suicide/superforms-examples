8e24821b2f4aa8a111779f6ebad7baa15f46001d
suiiii <suiiii@suiiii.rip>
2024-04-10 00:59:03 +0200

    added a page.ts for testing, same result

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,9 +2,9 @@ import type { LayoutLoad } from './$types';
 
 // export const prerender = false;
 
-export const load = (({ params }) => {
-  return {
-    foo: "kek"
-  };
-}) satisfies LayoutLoad;
+// export const load = (({ params }) => {
+//   return {
+//     foo: "kek"
+//   };
+// }) satisfies LayoutLoad;
 
---
+++ src/routes/+page.ts
@@ -0,0 +1,6 @@
+export const load = (({ params }) => {
+  return {
+    foo: "kek"
+  };
+});
+
